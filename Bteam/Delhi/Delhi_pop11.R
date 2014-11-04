9df31c61afca3082fa31c9276f0a0999a44106d0
katielong <yl3239@columbia.edu>
2014-11-04 15:20:49 -0500

    updates from before

--- Bteam/Delhi/Delhi_pop11.R
+++ Bteam/Delhi/Delhi_pop11.R
@@ -14,18 +14,26 @@ dis <- fortify(dis, region = "OBJECTID")
 # map <- map[,!(names(map) %in% drops)]
 
 
-
 data <- read.csv("Basic Info.csv")
 colnames(data)[4] <- "pop11"
+
+# test<-merge(data, dis, by.x="OBJECTID", by.y="id") # merge data frames (optional)
 
 # colourCount = length(unique(data$pop11))
 # getPalette = colorRampPalette(brewer.pal(9, "Blues"))
 
+# categorical fill
 ggplot()+
-  geom_map(data = data, aes(map_id = OBJECTID, fill= pop11, colour = DISTRICT),map=dis)+
-  geom_path(data=dis, aes(x=long, y=lat, group=group), color = NA, size = 0.5, linemetre = 0.5)+
-  coord_map(projection="mercator")+
-  labs(title="Population Census Result in 2011", fill="") +
+  geom_map(data = data, aes(map_id = OBJECTID, fill= DISTRICT, group = DISTRICT), map=dis) +
+  geom_path(data=dis, aes(x=long, y=lat, group=group), color = NA, size = 0.5, linemetre = 0.5) + 
+  scale_colour_brewer(type="seq", palette=length(levels(as.factor(data$DISTRICT))))
+
+#characteristic fill
+ggplot()+
+  geom_map(data = data, aes(map_id = OBJECTID, fill= pop11), map=dis) +
+  geom_path(data=dis, aes(x=long, y=lat, group=group), color = NA, size = 0.5, linemetre = 0.5) +
+  coord_map(projection="mercator") +
+  labs(title="Population Census Result in 2011") +
   theme(legend.position = "right")
 
 
